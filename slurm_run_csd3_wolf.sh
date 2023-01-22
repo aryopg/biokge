ac27c6b58ae54384a4aa31dc18b01fc70715c0e8
Aryo Gema <a.p.gema@sms.ed.ac.uk>
2023-01-22 14:46:03 +0000

    use lower nvidia version

--- slurm_run_csd3_wolf.sh
+++ slurm_run_csd3_wolf.sh
@@ -10,11 +10,12 @@
 #SBATCH -t 24:00:00  # time requested in hour:minute:seconds
 #SBATCH --cpus-per-task=3
 
-module load python-3.9.6-gcc-5.4.0-sbr552h cuda/11.2 cudnn/8.1_cuda-11.2 miniconda/3
-#module add cuda/11.0  # avoids compatibility complaint
-#module add cudnn/8.1_cuda-11.2
-#module add cuda/11.2
-#module add git-2.14.1-gcc-5.4.0-acb553e
+module add cuda/11.1 cudnn/8.0_cuda-11.1 miniconda/3
+# module add miniconda/3
+# module add cuda/11.0  # avoids compatibility complaint
+# module add cudnn/8.0_cuda-11.1
+# module add cuda/11.1
+# module add git-2.14.1-gcc-5.4.0-acb553e
 source ~/.bashrc
 conda init
 
@@ -33,10 +34,11 @@ mkdir -p ${SCRATCH_HOME}
 # Activate your conda environment
 CONDA_ENV_NAME=kge_playground
 echo "Setting up conda environment: ${CONDA_ENV_NAME}"
-conda create --name ${CONDA_ENV_NAME} python=3.9.6
+conda create --name ${CONDA_ENV_NAME} python=3.8
 conda activate ${CONDA_ENV_NAME}
-conda install -y -c conda-forge ogb wandb python-dotenv pre-commit black isort pydantic
-conda install -y pytorch torchvision torchaudio pytorch-cuda=11.2 -c pytorch -c nvidia
+conda install -y ogb wandb python-dotenv pre-commit black isort pydantic -c conda-forge
+#conda install -y python-dotenv -c conda-forge
+conda install -y pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
 
 echo "Moving input data to the compute node's scratch space: $SCRATCH_DISK"
 src_path=/home/${USER}/kge-playground/datasets/biokg
@@ -46,6 +48,7 @@ rsync --archive --update --compress --progress ${src_path}/ ${dest_path}
 
 echo "Running experiment"
 # limit of 12 GB GPU is hidden 256 and batch size 256
+which python
 python scripts/train.py \
 --config_filepath=$1
 --log_to_wandb
