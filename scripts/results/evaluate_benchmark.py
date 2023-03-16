1d496196c50b94ff378e673d45ffd2a71b9cd59e
wolfdewulf <s2408107@ed.ac.uk>
2023-03-16 18:51:29 +0000

    fixed triple classification code

--- scripts/results/evaluate_benchmark.py
+++ scripts/results/evaluate_benchmark.py
@@ -23,167 +23,104 @@ def evaluate(model_path):
     # Set seed
     random.seed(42)
 
-    # Create negative samples
-    def get_negatives(triples, others_triples, others_negatives, ratio=1):
+    def get_metrics_per_relation(relation):
+        valid_metrics = {}
+        test_metrics = {}
+
         def get_adjacency_matrix(triples):
-            matrix = numpy.full(
+            matrix = numpy.zeros(
                 (dataset._num_entities, dataset._num_entities),
-                fill_value=-1,
-                dtype=numpy.byte,
+                dtype=bool,
             )
-            matrix[triples[:, 0], triples[:, 2]] = triples[:, 1]
+            matrix[
+                triples[triples[:, 1] == relation, 0],
+                triples[triples[:, 1] == relation, 2],
+            ] = 1
             return matrix
 
-        adjacency_matrix = get_adjacency_matrix(triples)
-        adjacency_others = [
-            get_adjacency_matrix(other_triples) for other_triples in others_triples
-        ]
-
-        count = 0
-        negatives = set()
-        while count != int(len(triples) * ratio):
-            subject = random.randrange(dataset._num_entities)
-            object = random.randrange(dataset._num_entities)
-            relation = random.randrange(dataset._num_relations)
-            if (
-                (adjacency_matrix[subject, object] != relation)
-                and all(
-                    [
-                        adjacency_other[subject, object] != relation
-                        for adjacency_other in adjacency_others
-                    ]
-                )
-                and (subject, relation, object) not in negatives
-                and all(
-                    [
-                        (subject, relation, object) not in other_negatives
-                        for other_negatives in others_negatives
-                    ]
-                )
-            ):
-                negatives.add((subject, relation, object))
-                count += 1
-        return negatives
-
-    train_negatives = get_negatives(train_triples, [valid_triples, test_triples], [])
-    valid_negatives = get_negatives(
-        valid_triples, [train_triples, test_triples], [train_negatives]
-    )
-    test_negatives = get_negatives(
-        test_triples, [train_triples, valid_triples], [train_negatives, valid_negatives]
-    )
-
-    # Stack
-    train_negatives = torch.stack(
-        [
-            torch.tensor([subject, relation, object])
-            for subject, relation, object in train_negatives
-        ]
-    )
-    valid_negatives = torch.stack(
-        [
-            torch.tensor([subject, relation, object])
-            for subject, relation, object in valid_negatives
-        ]
-    )
-    test_negatives = torch.stack(
-        [
-            torch.tensor([subject, relation, object])
-            for subject, relation, object in test_negatives
-        ]
-    )
-
-    # Save negatives
-    numpy.savetxt(
-        "train_with_negatives.del",
-        torch.concatenate(
-            [
-                train_triples,
-                torch.stack(
-                    [
-                        torch.tensor([subject, dataset._num_relations, object])
-                        for subject, _, object in train_negatives
-                    ]
-                ),
+        # Create negative samples
+        def get_negatives(triples, others_triples, others_negatives, ratio=1):
+            adjacency_matrix = get_adjacency_matrix(triples)
+            adjacency_others = [
+                get_adjacency_matrix(other_triples) for other_triples in others_triples
             ]
-        ),
-        fmt="%i",
-        delimiter="\t",
-    )
-    numpy.savetxt(
-        "valid_with_negatives.del",
-        torch.concatenate(
+
+            count = 0
+            negatives = set()
+            while count != int(len(triples[:, 1] == relation) * ratio):
+                subject = random.randrange(dataset._num_entities)
+                object = random.randrange(dataset._num_entities)
+                other_relation = random.randrange(dataset._num_relations)
+                if (
+                    not adjacency_matrix[subject, object]
+                    and all(
+                        [
+                            not adjacency_other[subject, object]
+                            for adjacency_other in adjacency_others
+                        ]
+                    )
+                    and (subject, other_relation, object) not in negatives
+                    and all(
+                        [
+                            (subject, other_relation, object) not in other_negatives
+                            for other_negatives in others_negatives
+                        ]
+                    )
+                ):
+                    negatives.add((subject, other_relation, object))
+                    count += 1
+            return negatives
+
+        valid_negatives = get_negatives(
+            valid_triples, [train_triples, test_triples], []
+        )
+        test_negatives = get_negatives(
+            test_triples,
+            [train_triples, valid_triples],
+            [valid_negatives],
+        )
+
+        # Stack
+        valid_negatives = torch.stack(
             [
-                valid_triples,
-                torch.stack(
-                    [
-                        torch.tensor([subject, dataset._num_relations, object])
-                        for subject, _, object in valid_negatives
-                    ]
-                ),
+                torch.tensor([subject, relation, object])
+                for subject, relation, object in valid_negatives
             ]
-        ),
-        fmt="%i",
-        delimiter="\t",
-    )
-    numpy.savetxt(
-        "test_with_negatives.del",
-        torch.concatenate(
+        )
+        test_negatives = torch.stack(
             [
-                test_triples,
-                torch.stack(
-                    [
-                        torch.tensor([subject, dataset._num_relations, object])
-                        for subject, _, object in test_negatives
-                    ]
-                ),
+                torch.tensor([subject, relation, object])
+                for subject, relation, object in test_negatives
             ]
-        ),
-        fmt="%i",
-        delimiter="\t",
-    )
+        )
 
-    # Calculate metrics per relation
-    auc_roc = []
-    auc_prc = []
-    map = []
-    for relation in range(dataset._num_relations):
-
-        # Get positive triples
-        relation_valid_pos_triples = valid_triples[valid_triples[:, 1] == relation, :]
-        relation_test_pos_triples = test_triples[test_triples[:, 1] == relation, :]
-
-        # Get negative triples
-        relation_valid_neg_triples = valid_negatives[
-            random.sample(
-                range(0, len(valid_negatives)), len(relation_valid_pos_triples)
-            ),
-            :,
-        ]
-        relation_test_neg_triples = test_negatives[
-            random.sample(
-                range(0, len(test_negatives)), len(relation_test_pos_triples)
-            ),
-            :,
-        ]
+        # Save
+        numpy.savetxt(
+            "valid_negatives.del",
+            valid_negatives,
+            fmt="%i",
+            delimiter="\t",
+        )
+        numpy.savetxt(
+            "test_negatives.del",
+            test_negatives,
+            fmt="%i",
+            delimiter="\t",
+        )
 
         # Combine
-        relation_valid_triples = torch.vstack(
-            [relation_valid_pos_triples, relation_valid_neg_triples]
-        )
-        relation_test_triples = torch.vstack(
-            [relation_test_pos_triples, relation_test_neg_triples]
-        )
+        relation_valid_triples = torch.vstack([valid_triples, valid_negatives])
+        relation_test_triples = torch.vstack([test_triples, test_negatives])
         relation_valid_trues = torch.concatenate(
             [
-                torch.full((len(relation_valid_pos_triples),), 1),
-                torch.full((len(relation_valid_neg_triples),), 0),
+                torch.full((len(valid_triples),), 1),
+                torch.full((len(valid_negatives),), 0),
             ]
         )
         relation_test_trues = torch.concatenate(
             [
-                torch.full((len(relation_test_pos_triples),), 1),
-                torch.full((len(relation_test_neg_triples),), 0),
+                torch.full((len(test_triples),), 1),
+                torch.full((len(test_negatives),), 0),
             ]
         )
 
@@ -204,15 +141,11 @@ def evaluate(model_path):
         ## Calculate metrics
 
         # AUROC
-        auc_roc.append(
-            (
-                sklearn.metrics.roc_auc_score(
-                    relation_valid_trues, relation_valid_scores
-                ),
-                sklearn.metrics.roc_auc_score(
-                    relation_test_trues, relation_test_scores
-                ),
-            )
+        valid_metrics["auc_roc"] = sklearn.metrics.roc_auc_score(
+            relation_valid_trues, relation_valid_scores
+        )
+        test_metrics["auc_roc"] = sklearn.metrics.roc_auc_score(
+            relation_test_trues, relation_test_scores
         )
 
         # AUPRC
@@ -222,43 +155,46 @@ def evaluate(model_path):
         relation_test_prc = sklearn.metrics.precision_recall_curve(
             relation_test_trues, relation_test_scores
         )
-        auc_prc.append(
-            (
-                sklearn.metrics.auc(relation_valid_prc[1], relation_valid_prc[0]),
-                sklearn.metrics.auc(relation_test_prc[1], relation_test_prc[0]),
-            )
+        valid_metrics["auc_prc"] = sklearn.metrics.auc(
+            relation_valid_prc[1], relation_valid_prc[0]
+        )
+        test_metrics["auc_prc"] = sklearn.metrics.auc(
+            relation_test_prc[1], relation_test_prc[0]
         )
 
         # MAP
-        map.append(
-            (
-                sklearn.metrics.average_precision_score(
-                    relation_valid_trues, relation_valid_scores
-                ),
-                sklearn.metrics.average_precision_score(
-                    relation_test_trues, relation_test_scores
-                ),
-            )
+        valid_metrics["map"] = sklearn.metrics.average_precision_score(
+            relation_valid_trues, relation_valid_scores
         )
+        test_metrics["map"] = sklearn.metrics.average_precision_score(
+            relation_test_trues, relation_test_scores
+        )
+
+        return valid_metrics, test_metrics
+
+    # Get metrics per relation
+    metrics = [
+        get_metrics_per_relation(relation) for relation in range(dataset._num_relations)
+    ]
 
     # Print relation average
     print(
-        f"Validation AUROC = {sum([valid_auc_roc for valid_auc_roc, _ in auc_roc]) / dataset._num_relations}"
+        f"Validation AUROC = {sum([valid_metrics['auc_roc'] for valid_metrics, _  in metrics]) / dataset._num_relations}"
     )
     print(
-        f"Testing AUROC    = {sum([test_auc_roc for _, test_auc_roc in auc_roc]) / dataset._num_relations}"
+        f"Testing AUROC    = {sum([test_metrics['auc_roc'] for _, test_metrics in metrics]) / dataset._num_relations}"
     )
     print(
-        f"Validation AUPRC = {sum([valid_auc_prc for valid_auc_prc, _ in auc_prc]) / dataset._num_relations}"
+        f"Validation AUPRC = {sum([valid_metrics['auc_prc'] for valid_metrics, _ in metrics]) / dataset._num_relations}"
     )
     print(
-        f"Testing AUPRC    = {sum([test_auc_prc for _, test_auc_prc in auc_prc]) / dataset._num_relations}"
+        f"Testing AUPRC    = {sum([test_metrics['auc_prc'] for _, test_metrics in metrics]) / dataset._num_relations}"
     )
     print(
-        f"Validation MAP   = {sum([valid_map for valid_map, _ in map]) / dataset._num_relations}"
+        f"Validation MAP   = {sum([valid_metrics['map'] for valid_metrics, _ in metrics]) / dataset._num_relations}"
     )
     print(
-        f"Testing MAP      = {sum([test_map for _, test_map in map]) / dataset._num_relations}"
+        f"Testing MAP      = {sum([test_metrics['map'] for _, test_metrics in metrics]) / dataset._num_relations}"
     )
 
 
